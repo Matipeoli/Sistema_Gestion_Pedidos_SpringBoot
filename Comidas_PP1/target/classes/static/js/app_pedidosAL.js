21fa0f3c9f463025007504d81a7c19b8b5fe76d5
Manuel Canello <canellomanuel2@gmail.com>
2025-11-01 01:36:00 -0300

    eliminacion de resources

--- Comidas_PP1/target/classes/static/js/app_pedidosAL.js
+++
@@ -1,9 +0,0 @@
-<section>
-  <h2>Pedidos – Aromas Light</h2>
-
-  <div class="controls">
-    <button onclick="history.back()">← Volver</button>
-  </div>
-
-  <div id="pedidos-aromas" class="grid grid-3"></div>
-</section>
--- Comidas_PP1/target/classes/static/js/cargar_menus.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    new Calendario('month-select', 'year-select', 'calendar-grid','calendario','cargar');
-});
